959ddf24763b3e604d006203319602d3d5fe3a25
oyzh888 <4d:c2:b4:74:55:73:24:77:43:b3:d0:18:82:e8:7d:8f>
2018-10-16 16:56:47 +0800

    add pic to index.md and change to polyphony model

--- code/monophonic/monophony.py
+++ code/monophonic/monophony.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+# -*- coding:UTF-8 -*-
 
 from __future__ import print_function
 from keras import backend as K
--- code/monophonic/my_to_midi.py
+++ code/monophonic/my_to_midi.py
@@ -1,3 +1,5 @@
+# -*- coding:UTF-8 -*-
+
 # Copyright 2016 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
